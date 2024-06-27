f0d06450083ccb47ccf5938ea9c39232fed5445e
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 14:06:54 +0000

    bug:resolving conflicts

--- dist/server.js
+++ dist/server.js
@@ -4,8 +4,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+require("dotenv/config");
+const job_routes_1 = __importDefault(require("./src/routes/job.routes"));
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
 const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+const MONGO_URL = process.env.MONGO_URL;
+app.get('/', (request, response) => {
+    response.send("Hello There");
 });
+app.use('/api/jobs', job_routes_1.default);
+mongoose_1.default.connect(MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}).catch((err) => console.log("Failed to connect to database", err));
