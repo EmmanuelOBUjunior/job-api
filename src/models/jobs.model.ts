eba5c466f23ec975bfd507ba415563f67cd5d4c1
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 10:55:10 +0000

    feat:working on jobs model

--- src/models/jobs.model.ts
+++ src/models/jobs.model.ts
@@ -1,1 +1,18 @@
 import mongoose from "mongoose";
+
+const JobSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required: true
+    },
+    type:{
+        type:String,
+        required: true
+    },
+    description:{
+        type:String,
+        required:true
+    }
+},{
+    timestamps:true
+})
