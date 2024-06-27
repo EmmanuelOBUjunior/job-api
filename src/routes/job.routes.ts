b04bc2fdfefa7c1822d14aaa55727d8f3a6902a4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 11:21:25 +0000

    feat:working on createJob controller

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { getJobs } from "../controllers/jobs.controller";
+import { createJob, getJobs } from "../controllers/jobs.controller";
 
 const router = Router()
 
 router.get('/', getJobs)
-router.get('/', createJob)
+router.post('/', createJob)
 
 
 
