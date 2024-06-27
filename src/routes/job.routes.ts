6a16ef99b2f26c09929e55841f580ef668f057fa
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 11:38:50 +0000

    feat:working on getjobbyid controller

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createJob, getJobs } from "../controllers/jobs.controller";
+import { createJob, getJobById, getJobs } from "../controllers/jobs.controller";
 
 const router = Router()
 
 router.get('/', getJobs)
+router.get('/:id', getJobById)
 router.post('/', createJob)
 
 
