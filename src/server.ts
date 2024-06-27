72d33b8d580ff0a3dc1f83043aabc1296b0b66db
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 10:42:27 +0000

    feat:connecting to database

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose from 'mongoose'
 import 'dotenv/config'
 import jobsRouter from './routes/job.routes'
 
@@ -7,9 +8,14 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL 
 
 app.get('/api/jobs', jobsRouter)
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
 })
+
+mongoose.connect(MONGO_URL).then(()=>{
+
+}).catch((err)=> console.log("Failed to connect to database", err))
