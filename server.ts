c4f39af935ffe3b1b93951f7efc8ce8b6690d6e4
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 12:17:12 +0000

    bug:resolving conflicts

---
+++ server.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import { Request, Response } from 'express-serve-static-core'
+import mongoose from 'mongoose'
+import 'dotenv/config'
+import jobsRouter from './routes/job.routes'
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+const PORT = process.env.PORT
+const MONGO_URL = process.env.MONGO_URL! 
+
+app.get('/', (request:Request, response:Response) =>{
+    response.send("Hello There")
+})
+
+app.use('/api/jobs', jobsRouter)
+
+mongoose.connect(MONGO_URL).then(()=>{
+    console.log('Connected to MongoDB')
+    app.listen(PORT, ()=>{
+        console.log(`Server running on port ${PORT}`)
+    })
+}).catch((err)=> console.log("Failed to connect to database", err))
