2724e3b9e2247026615081d6c4a5ba2634b89384
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 10:28:44 +0000

    feat:setting up job router

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import jobsRouter from './routes/job.routes'
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded())
+app.use(express.urlencoded({extended:true}))
 const PORT = process.env.PORT
 
 app.get('/api/jobs', jobsRouter)
