f03c6e85d506bea8ef6565b32995d31874cb11bc
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2024-06-27 11:44:38 +0000

    feat:working on updatejob contoller

--- src/routes/job.routes.ts
+++ src/routes/job.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createJob, getJobById, getJobs } from "../controllers/jobs.controller";
+import { createJob, getJobById, getJobs, updateJob } from "../controllers/jobs.controller";
 
 const router = Router()
 
 router.get('/', getJobs)
 router.get('/:id', getJobById)
 router.post('/', createJob)
+router.put('/:id', updateJob)
 
 
 
